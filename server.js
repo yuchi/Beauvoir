f7ee4a1d381507ada392a35813d0098c6b1cda59
Pier <pier@black-alloy.homenet.telecomitalia.it>
2011-04-18 22:46:01 +0200

    Stupid edits

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var http = require('http');
 
 http.createServer(function (req, res) {
 
-	res.send('Hallo, world. This is Pier Paolo speaking. Hi to all.');
+	res.write('Hallo, world. This is Pier Paolo speaking. Hi to all.');
+	res.end();
 
 }).listen(80);
 
